38493aec9c41671842e977dfbca0b829fc6a76d1
Retr0 <sebasmorandez@hotmail.com>
2022-09-16 11:56:14 -0500

    create main class and menu function

--- src/App.java
+++ src/App.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        App app = new App();
+        System.out.println(app.menu());
+
+    }
+
+    public String menu(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingresa Algoritmo de Busqueda: ");
+        try {
+            int choice = input.nextInt();
+
+            input.close();
+
+            switch (choice) {
+                case 1:{
+                    return new String("Busqueda en Profundidad");
+                    // break;
+                }
+                case 2:{
+                    return new String("Busqueda en Amplitud");
+                    // break;
+                }
+                case 3:{
+                    return new String("Escalado de Colina");
+                    // break;
+                }
+                case 4:{
+                    return new String("Primero el Mejor");
+                    // break;
+                }
+                case 5:{
+                    return new String("A*");
+                    // break;
+                }
+                default:
+                    return new String("Algoritmo no valido.");
+                    // break;
+            }
+        } catch (Exception e) {
+            // e.printStackTrace();
+            return new String("Valor ingresado no valido.");
+        }
     }
 }
